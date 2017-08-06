4279402d03bb44ad290e98e0ec8a55876ae3a3e8
dkarpushkin <dmitry.karpushkin.true@gmail.com>
2017-08-07 00:22:56 +0500

    db

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    if os.environ.get("DEBUG") == True:
+    if os.environ.get("HEROKU"):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.heroku")
+    else:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.vagrant")
 
     from django.core.management import execute_from_command_line
--- settings/heroku.py
+++ settings/heroku.py
@@ -4,20 +4,21 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') is not None and os.environ.get('DEBUG').lower() == 'true'
 
-PLATFORM = 'heroku'
-
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config("DATABASE_URL"),
+    'eve_db': dj_database_url.config("HEROKU_POSTGRESQL_WHITE_URL"),
 }
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
